2fe1edafa8cacd3d7009e5e7350b525c7dc8f2da
newtonick <nick@klockenga.net>
2026-07-18 23:50:32 -0400

    first

---
+++ blaze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  blaze
+//
+//  Created by Nick Klockenga on 7/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ blaze/blazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  blazeApp.swift
+//  blaze
+//
+//  Created by Nick Klockenga on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct blazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
